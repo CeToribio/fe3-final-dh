70bce04967575df2492d9ddd1321557bc2828d0c
CeToribio <anctoribio@gmail.com>
2024-09-07 23:37:42 -0500

    implement page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Detail from "./Pages/Detail";
-import Contact from "./Pages/Contact";
+import Contact from "./Pages/Contact/Contact";
 import Favs from "./Pages/Favs";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import { routes } from "./utils/routes";
+import ErrorPageNotFound from "./Pages/Error_404/Error_404";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Route path={routes.dentist} element={<Detail />} />
         <Route path={routes.contact} element={<Contact />} />
         <Route path={routes.favs} element={<Favs />} />
-        <Route path={routes.notFound} element={<h2>Error 404 - Page not found</h2>} />
+        <Route path={routes.notFound} element={<ErrorPageNotFound />} />
       </Routes>
       <Footer />
     </div>
