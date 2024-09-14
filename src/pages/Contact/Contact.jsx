69691e22c57f1e685fbccbf60b60c6fb82e435f1
CeToribio <anctoribio@gmail.com>
2024-09-14 01:25:15 -0500

    implement styles to theme

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,17 +1,19 @@
 //import React from 'react'
-import Form from '../../Components/Form/Form'
-import './contact.css'
+import Form from "../../Components/Form/Form";
+import { useContextGlobal } from "../../Context/global.context";
+import "./contact.css";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
+  const { theme } = useContextGlobal();
   return (
-    <div className='div-contact'>
-      <h2>¿Quieres saber más?</h2>
-      <p>Envíanos tus consultas y nos pondremos en contacto contigo</p>
-      <Form/>
+    <div className={`div-contact-${theme}`}>
+      <h2 className={`div-contact-p-${theme}`}>¿Quieres saber más?</h2>
+      <p className={`div-contact-p-${theme}`}>Envíanos tus consultas y nos pondremos en contacto contigo</p>
+      <Form />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
